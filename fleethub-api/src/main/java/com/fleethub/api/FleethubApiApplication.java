e20cf771bb33b69a4ad3759158aa50be0aa2b836
CarlosMg96 <rguezcarlosmg96@outlook.com>
2026-08-23 16:43:59 -0600

    add java app

---
+++ fleethub-api/src/main/java/com/fleethub/api/FleethubApiApplication.java
@@ -0,0 +1,13 @@
+package com.fleethub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleethubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleethubApiApplication.class, args);
+	}
+
+}
---
+++ fleethub-api/src/test/java/com/fleethub/api/FleethubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleethub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleethubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
